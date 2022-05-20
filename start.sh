ad53df10a2ec72fe52fa842425a80dc88fcc6538
M SHAH <shah@idk.uno>
2022-05-20 15:31:59 +0100

    💥 ✨ docker will always check for update on restart unless `--no-update` is set #43

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 echo "Starting the application"
 echo $@
-if [[ $@ == *"--always-latest"* ]]; then
-  eval "PUPPETEER_SKIP_DOWNLOAD=true npm i @open-wa/wa-automate@latest --ignore-scripts"
+if [[ $@ == *"--no-update"* ]]; then
+  echo "Skipping update"
+  else eval "PUPPETEER_SKIP_DOWNLOAD=true npm i @open-wa/wa-automate@latest --ignore-scripts"
 fi
 exec node ./node_modules/@open-wa/wa-automate/bin/server.js $@
